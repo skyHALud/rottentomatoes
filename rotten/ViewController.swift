373773965c442134b8ba69edcad5ab74f757ffa1
Swaroop Butala <swaroop@box.com>
2014-09-10 20:02:27 -0700

    Initial Commit

---
+++ rotten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rotten
+//
+//  Created by Swaroop Butala on 9/10/14.
+//  Copyright (c) 2014 Swaroop Butala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
