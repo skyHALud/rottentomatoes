5265afd74e4d4440dd5f47ac1cc22968c6e64e7b
Swaroop Butala <swaroop@box.com>
2014-09-13 21:12:28 -0700

    Added animation to the view

--- rotten/MovieDetailViewController.swift
+++ rotten/MovieDetailViewController.swift
@@ -14,17 +14,21 @@ class MovieDetailViewController: UIViewController {
         super.init(coder: aDecoder)
     }
 
+    @IBOutlet weak var movieDetailView: UIView!
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var movieTitleLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
     @IBOutlet weak var tomatoScoreLabel: UILabel!
     var movie: NSDictionary?
+    
+    var movieDetailViewTrayOpen = false
+    var movieDetailViewOriginalPosition = 0
+
+    @IBOutlet var movieTitleTapRecognizer: UITapGestureRecognizer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        posterImageView.setImageWithURL(NSURL(string: "http://content7.flixster.com/movie/11/17/88/11178889_tmb.jpg"))
-        posterImageView.setImageWithURL(NSURL(string: "http://content7.flixster.com/movie/11/17/88/11178889_ptr.jpg"))
 
 //        var request = NSURLRequest(URL: NSURL(string: "http://content7.flixster.com/movie/11/17/88/11178889_tmb.jpg"))
 
@@ -39,16 +43,41 @@ class MovieDetailViewController: UIViewController {
 //        AFImageCache.cachedImageForRequest(request)
         // Do any additional setup after loading the view.
         
+        // Set all the information about the movies
+        var posters = self.movie!["posters"] as NSDictionary
+        var posterUrl = posters["thumbnail"] as String
+        let originalUrl = posterUrl.stringByReplacingOccurrencesOfString("tmb", withString: "ori")
+        
+        posterImageView.setImageWithURL(NSURL(string: originalUrl))
+
         movieTitleLabel.text = self.movie!["title"] as? String
         ratingLabel.text = self.movie!["mpaa_rating"] as? String
         synopsisLabel.text = self.movie!["synopsis"] as? String
+        synopsisLabel.numberOfLines = 0
+        synopsisLabel.sizeToFit()
         var ratings = movie!["ratings"] as NSDictionary
         var criticsScore = ratings["critics_score"] as NSInteger
         var audienceScore = ratings["audience_score"] as NSInteger
         tomatoScoreLabel.text = "Critics Score: \(criticsScore) Audience Score: \(audienceScore)"
+        movieTitleTapRecognizer.addTarget(self, action: "tappedMovieTitleView")
         
     }
 
+    func tappedMovieTitleView() {
+        UIView.animateWithDuration(0.4, animations: {
+            // Move the search bar up by its height, and hide it
+            var movieDetailViewFrame = self.movieDetailView.frame
+            if (!self.movieDetailViewTrayOpen) {
+                self.movieDetailViewOriginalPosition = Int(movieDetailViewFrame.origin.y)
+                movieDetailViewFrame.origin.y = 60
+                self.movieDetailViewTrayOpen = true
+            } else {
+                movieDetailViewFrame.origin.y = CGFloat(self.movieDetailViewOriginalPosition)
+                self.movieDetailViewTrayOpen = false
+            }
+            self.movieDetailView.frame = movieDetailViewFrame
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
